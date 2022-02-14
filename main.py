9a493961d2d5b1bd41681212e929d1bca47b2a40
MRWAF <eric.vicente00@gmail.com>
2022-02-14 15:44:37 -0500

    xd

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 import pandas as pd
-from numpy import absolute, mean, std
-import matplotlib.pyplot as plt
-from sklearn import linear_model
-from sklearn.model_selection import RepeatedKFold, cross_val_score
-from sklearn.linear_model import  Lasso
+import gplearn
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('forestfires.csv', sep = ',', encoding='latin1')
-data = df.values
-print(data)
+
+X, y = train_test_split(df, test_size=.2, random_state= 2)
+
+Y_train = X.iloc[:,-1].values
 
 
-X, y = data[:, :-1], data[:, -1]
-print(y)
-las = Lasso(alpha= 1.0)
-
-cv = RepeatedKFold(n_splits= 10, n_repeats= 3, random_state= 1)
-
-scores = cross_val_score(las, X, y, scoring= 'neg_mean_absolute_error', cv = cv, n_jobs = -1)
-scores = absolute(scores)
-
-
-
+#https://www.mygreatlearning.com/blog/understanding-of-lasso-regression/
